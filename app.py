ed45aaad8ac6a9577f09e18a761b6d7e43967414
jakemarkd3v <jakemarkd3v@gmail.com>
2022-11-06 13:40:09 +0800

    added files

--- app.py
+++ app.py
@@ -2,13 +2,32 @@ import json
 from difflib import get_close_matches
 data = json.load(open("data.json"))
 
-def translate(word):
-    word =  word.lower()
+def translate(w):
+    word =  w.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(w, data.keys()))> 0:
+        yn = input("Did you mean %s ?  Enter Y if yes, or N if no: \n" % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return " The word doesn't exist. Please check it. "
+        else:
+            return " I didn't understand your entry."
+
     else:
-        return "The word doesn't exist. Did you mean" + " "+ get_close_matches(word, data.keys())[0] + "?"
+        return "The word doesn't exist. Did you mean"
 
-word = input("Enter a word: ")
+w = input("Enter a word: ")
 
-print (translate(word))
+output = translate(w)
+
+if type(output) == list:
+    for i in output:
+        print (i)
+
+else:
+    print(output)
+
+
+
